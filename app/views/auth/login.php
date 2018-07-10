175c6e59d2a00c595fad2c172351031953754707
Raymund Santillan <raymsantillan@gmail.com>
2018-07-10 23:22:32 +0800

    Fix placeholders

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -15,13 +15,13 @@
           <div class="form-group row">
             <label for="inputEmail3" class="col-sm-2 form-control-label">Username</label>
             <div class="col-sm-10">
-              <input name="username" type="text" class="form-control" id="inputEmail3" placeholder="Email">
+              <input name="username" type="text" class="form-control" id="inputEmail3" placeholder="Username">
             </div>
           </div>
           <div class="form-group row">
             <label for="inputPassword3" class="col-sm-2 form-control-label">Password</label>
             <div class="col-sm-10">
-              <input  name="password" type="password" class="form-control" id="inputPassword3" placeholder="Email">
+              <input  name="password" type="password" class="form-control" id="inputPassword3" placeholder="Password">
             </div>
           </div>
           
--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 	//load the bootstap config
     include __DIR__.'/./bootstrap/bootstrap.php';
+    include __DIR__.'/./routes.php';
 
  ?>
